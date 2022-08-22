51a139652d36f70bd4e1832dd1f7f8a3debcb0da
gbencito <107825165+gbencito@users.noreply.github.com>
2022-08-22 08:27:07 +0800

    Add files via upload

---
+++ changerootopass.sh
@@ -0,0 +1,3 @@
+
+# ansible-playbook changerootpassword.yaml --extra-vars newpassword=Password01 --limit TestLinux --ask-pass
+ansible-playbook changerootpassword.yaml --extra-vars newpassword=2022L1nuX@S0L@!rE --ask-pass
